858e6450ff5837b56f3b5eb628df9e0a33abef6d
NiLuJe <ninuje@gmail.com>
2021-06-12 21:31:27 +0200

    Import Geom

--- nanoclock.lua
+++ nanoclock.lua
@@ -28,6 +28,7 @@ require("ffi/posix_h")
 -- FIXME: Mangle package loading so I can keep this in lib/
 local lfs = require("lfs")
 local util = require("lib/util")
+local Geom = require("lib/geometry")
 local INIFile = require("lib/inifile")
 local FBInk = ffi.load("lib/libfbink.so.1.0.0")
 
