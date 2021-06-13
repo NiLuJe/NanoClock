5bf4a3d00f16e058b606ebf8910970ac6097973a
NiLuJe <ninuje@gmail.com>
2021-06-14 00:29:02 +0200

    Only update the marker tracking on successful prints

--- nanoclock.lua
+++ nanoclock.lua
@@ -197,13 +197,21 @@ function NanoClock:displayClock()
 	local ret = FBInk.fbink_print(self.fbink_fd, self.clock_string, self.fbink_cfg)
 	if ret < 0 then
 		logger.warn("FBInk failed to display the string `%s`", self.clock_string)
-	end
+	else
+		-- NOTE: On failure, FBInk's own marker will have incremented,
+		--       but a a failure means we'll potentially never have a chance to *ever* caught a damage event for it,
+		--       depending on where in the chain of events the failure happened.
+		--       (i.e., if it's actually the *ioctl* that failed, we *would* catch it,
+		--       but any earlier and there won't be any ioctl at all ;)).
+		--       TL;DR: Only update the marker tracking on *success*.
 
-	-- Remember our marker to be able to ignore its damage event, otherwise we'd be stuck in an infinite loop ;).
-	self.clock_marker = FBInk.fbink_get_last_marker()
-	logger.dbg("Updated clock (marker: %u)", ffi.cast("unsigned int", self.clock_marker))
-	-- Reset the damage tracker
-	self.marker_found = false
+		-- Remember our marker to be able to ignore its damage event, otherwise we'd be stuck in an infinite loop ;).
+		-- c.f., the whole logic in :waitForEvent().
+		self.clock_marker = FBInk.fbink_get_last_marker()
+		logger.dbg("Updated clock (marker: %u)", ffi.cast("unsigned int", self.clock_marker))
+		-- Reset the damage tracker
+		self.marker_found = false
+	end
 
 	-- Remember our damage area to detect if we actually need to repaint
 	local rect = FBInk.fbink_get_last_rect()
