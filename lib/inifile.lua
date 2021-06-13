eafff5f760b7da8176549e8d1d035a4bb41f5873
NiLuJe <ninuje@gmail.com>
2021-06-13 06:15:15 +0200

    Trim the fat from inifile
    Don't need to a write handle,r so we can drop the comments & metatable

--- lib/inifile.lua
+++ lib/inifile.lua
@@ -47,21 +47,10 @@ local backends = {
 	},
 }
 
-if love then
-	backends.love = {
-		lines = love.filesystem.lines,
-		write = function(name, contents) love.filesystem.write(name, contents) end,
-	}
-	defaultBackend = "love"
-end
-
 function inifile.parse(name, backend)
 	backend = backend or defaultBackend
 	local t = {}
 	local section
-	local comments = {}
-	local sectionorder = {}
-	local cursectionorder
 
 	for line in backends[backend].lines(name) do
 
@@ -70,16 +59,6 @@ function inifile.parse(name, backend)
 		if s then
 			section = s
 			t[section] = t[section] or {}
-			cursectionorder = {name = section}
-			table.insert(sectionorder, cursectionorder)
-		end
-
-		-- Comments
-		s = line:match("^;(.+)$")
-		if s then
-			local commentsection = section or comments
-			comments[commentsection] = comments[commentsection] or {}
-			table.insert(comments[commentsection], s)
 		end
 
 		-- Key-value pairs
@@ -89,97 +68,10 @@ function inifile.parse(name, backend)
 		if value == "false" then value = false end
 		if key and value ~= nil then
 			t[section][key] = value
-			table.insert(cursectionorder, key)
 		end
 	end
 
-	-- Store our metadata in the __inifile field in the metatable
-	return setmetatable(t, {
-		__inifile = {
-			comments = comments,
-			sectionorder = sectionorder,
-		}
-	})
-end
-
-function inifile.save(name, t, backend)
-	backend = backend or defaultBackend
-	local contents = {}
-
-	-- Get our metadata if it exists
-	local metadata = getmetatable(t)
-	local comments, sectionorder
-
-	if metadata then metadata = metadata.__inifile end
-	if metadata then
-		comments = metadata.comments
-		sectionorder = metadata.sectionorder
-	end
-
-	-- If there are comments before sections,
-	-- write them out now
-	if comments and comments[comments] then
-		for i, v in ipairs(comments[comments]) do
-			table.insert(contents, (";%s"):format(v))
-		end
-		table.insert(contents, "")
-	end
-
-	local function writevalue(section, key)
-		local value = section[key]
-		-- Discard if it doesn't exist (anymore)
-		if value == nil then return end
-		table.insert(contents, ("%s=%s"):format(key, tostring(value)))
-	end
-
-	local function writesection(section, order)
-		local s = t[section]
-		-- Discard if it doesn't exist (anymore)
-		if not s then return end
-		table.insert(contents, ("[%s]"):format(section))
-
-		-- Write our comments out again, sadly we have only achieved
-		-- section-accuracy so far
-		if comments and comments[section] then
-			for i, v in ipairs(comments[section]) do
-				table.insert(contents, (";%s"):format(v))
-			end
-		end
-
-		-- Write the key-value pairs with optional order
-		local done = {}
-		if order then
-			for _, v in ipairs(order) do
-				done[v] = true
-				writevalue(s, v)
-			end
-		end
-		for i, _ in pairs(s) do
-			if not done[i] then
-				writevalue(s, i)
-			end
-		end
-
-		-- Newline after the section
-		table.insert(contents, "")
-	end
-
-	-- Write the sections, with optional order
-	local done = {}
-	if sectionorder then
-		for _, v in ipairs(sectionorder) do
-			done[v.name] = true
-			writesection(v.name, v)
-		end
-	end
-	-- Write anything that wasn't ordered
-	for i, _ in pairs(t) do
-		if not done[i] then
-			writesection(i)
-		end
-	end
-
-	return backends[backend].write(name, table.concat(contents, "\n"))
+	return t
 end
 
 return inifile
